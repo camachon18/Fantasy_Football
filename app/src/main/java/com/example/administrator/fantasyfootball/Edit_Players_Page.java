f707b06b60dc28d3e94b77224328ac97dafd729b
camachon18 <camachon18@up.edu>
2015-09-19 16:05:30 -0700

    Initial commit

---
+++ app/src/main/java/com/example/administrator/fantasyfootball/Edit_Players_Page.java
@@ -0,0 +1,56 @@
+package com.example.administrator.fantasyfootball;
+
+import android.content.Intent;
+import android.support.v7.app.ActionBarActivity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+
+
+public class Edit_Players_Page extends ActionBarActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_edit__players__page);
+
+        goBackToMainMenu();
+    }
+
+    private void goBackToMainMenu()
+    {
+        Button switchToMainMenu = (Button) findViewById(R.id.toMainMenu1);
+        switchToMainMenu.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                finish();
+            }
+        });
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_edit__players__page, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+
+
+}
